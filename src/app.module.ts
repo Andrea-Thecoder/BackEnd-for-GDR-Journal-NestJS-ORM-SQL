d62ca2dcd07c949a62ba7882d2bfcff1867cc563
Andrea-Thecoder <paldemar8@gmail.com>
2024-09-17 17:59:43 +0200

    add throtter guard, remove limit-rate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,19 @@ import { UsersController } from './users/controller/users.controller';
 import { UsersModule } from './users/users.module'; 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserProfileModule } from './user-profile/user-profile.module';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { JournalModule } from './journal/journal.module';
 import { AuthModule } from './auth/auth.module';
 import { JournalPageModule } from './journal-page/journal-page.module';
 import { RateLimiterModule } from 'nestjs-rate-limiter'; //serve per limitare il numero di rciheste in u ndetermianto arco temporale, aiutando cosi nella prevenzione da attacchi DoS! può essere anche usato tramite decorator nei controller
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60, //in in millisecondi
+      limit: 10, //limite di ping allo stesso endpoint nll'arco di tempo sopracitato.
+    }]),
     TypeOrmModule.forRoot({
       type: 'mysql', //type di DB usato
       host: "localhost", //l'host del server, sottoforma di IP o domain
@@ -24,11 +29,6 @@ import { RateLimiterModule } from 'nestjs-rate-limiter'; //serve per limitare il
       //synchronize : true //USARE SOLO IN SVILUPPO! crea i schemi ogni qualvolta vnegano creati qui in ambiente di sviluppo.
       //logging: true //serve per attivare il log nel terminare
     }),
-    RateLimiterModule.register({
-      for: 'Express',  //tipo di app che sta girando, nel nostro caso express
-      points: 10, //numero di richieste fattibili nell'arco temporale definito
-      duration: 60, //arco temporale definito per il nmreo di points, espresso in secondi
-    }),
     UsersModule,
     UserProfileModule,
     JournalModule,
@@ -39,7 +39,10 @@ import { RateLimiterModule } from 'nestjs-rate-limiter'; //serve per limitare il
   providers: [{
     provide: APP_PIPE,
     useClass: ValidationPipe,
-
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
     AppService
   ],
